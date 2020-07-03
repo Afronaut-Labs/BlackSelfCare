5ed47918a854e08d0fa7c9c651391ae5308aef24
travellwilliams <travellwilliams11@GMAIL.com>
2020-07-02 19:03:27 -0500

    Added images and quotes components + styling

---
+++ black-self-care/src/Images.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Images() {
+    return(
+        <div class="">
+            <img src="./public/images/quote1.jpg"></img>
+            <img src="https://i.pinimg.com/originals/87/be/d3/87bed3649b2945a9efa38f6c54daa637.jpg"></img>
+        </div>
+    );
+}
+
+export default Images;
---
+++ black-self-care/src/Quotes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Quotes() {
+    return(
+        <div class="text-container">
+            <p class="text">All that you touch. You Change. All that you Change Changes you.</p>
+            <p class="text">Do what you love</p>
+            <p class="text">Difficult roads lead to beautiful destinations</p>
+            <p class="text">Black is beautiful</p>
+        </div>
+    );
+}
+
+export default Quotes;
+
+
